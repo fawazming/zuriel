0bfc6368235f5214a27a214b68b3213e7fadeaf4
adesanya001 <ajewoleayomide30@gmail.com>
2024-09-23 00:22:44 +0100

    Dark Theme Added

---
+++ ayomide/tailwindPractice3/script.js
@@ -0,0 +1,16 @@
+function theme() {
+    // document.querySelector('#themeIcon').classList.toggle('src = `https://icongr.am/clarity/moon.svg?size=20&color=9ca3af`');
+    document.querySelector('body').classList.toggle('bdy');
+    document.querySelector('#head').classList.toggle('head');
+    document.querySelector('#box1').classList.toggle('box');
+    document.querySelector('#box2').classList.toggle('box');
+    document.querySelector('#box3').classList.toggle('box');
+    document.querySelector('#box4').classList.toggle('box');
+    document.querySelector('#box5').classList.toggle('box');
+    document.querySelector('#box6').classList.toggle('box');
+    document.querySelector('#box7').classList.toggle('box');
+    document.querySelector('#box8').classList.toggle('box');
+    document.querySelector('#text1').classList.toggle('text');
+    document.querySelector('#text2').classList.toggle('text');
+    document.querySelector('#supportUs').classList.toggle('support');
+}
